46832add25bbeba984f048c393212ecec04ad77b
Christina Chortaria <actspatial@gmail.com>
2026-06-15 17:09:08 -0400

    Install pytest and httpx for testing Encode method add test

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_encode_text():
+    response = client.get("/embedding/Hello%20World")
+    assert response.status_code == 200
+    data = response.json()
+    assert "embedding_binary" in data
+    assert isinstance(data["embedding_binary"], list)
